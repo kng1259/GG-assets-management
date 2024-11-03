796812a49b5152cc48aa4e3b560faea0fd7393d0
kng1259 <khoinguyenanlac1@gmail.com>
2024-11-03 13:31:56 +0700

    add user get assets route

--- backend/src/main/java/com/gg/assets/assets_management/asset/AssetRepository.java
+++ backend/src/main/java/com/gg/assets/assets_management/asset/AssetRepository.java
@@ -2,8 +2,9 @@ package com.gg.assets.assets_management.asset;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface AssetRepository extends JpaRepository<Asset, Long> {
-
+    List<Asset> findByDepartmentId(Long departmentId);
 }
