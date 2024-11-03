b5980cf911ae6dffff5e7687312d9b646a01c5cd
viLam11 <ngoc.huynhcs2212257@hcmut.edu.com>
2024-11-03 13:26:46 +0700

    ADD REPONSE API

--- backend/src/main/java/com/gg/assets/assets_management/department/DepartmentController.java
+++ backend/src/main/java/com/gg/assets/assets_management/department/DepartmentController.java
@@ -1,6 +1,10 @@
 package com.gg.assets.assets_management.department;
 
+import com.gg.assets.assets_management.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin("*")
@@ -10,12 +14,29 @@ public class DepartmentController {
     @Autowired
     DepartmentService departmentService;
 
-    @PostMapping("/create")
-    public Department createDepartment(@RequestBody Department department) {
-        // Process the request body here, such as saving to the database
-        return departmentService.createDept(department);
-    }
 
+    // Process the request body here, such as saving to the database
+//        Department newDept = new Department(deptName);
+//    public ResponseEntity<ApiResponse<Department>> Department result = departmentService.createDept(payLoad);
+//        if(result == null) {
+//            ResponseEntity res = new ReponseEntity(400, "Cannot create");
+//            return res;
+//        } else {
+//            ReponseEntity res = new ReponseEntity(200, "success");
+//            return res;
+//        }
+    @PostMapping("/create")
+    public ResponseEntity<ApiResponse<Department>> createDept(@RequestBody Department newDept) {
+        Department result = departmentService.createDept(newDept);
+        System.out.print(result) ;
+        if(result == null) {
+            ApiResponse<Department> reponse = new ApiResponse<>(400, "Cannot create", null);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(reponse);
+        } else {
+            ApiResponse<Department> reponse = new ApiResponse<>(200,"Create dept success", result );
+            return ResponseEntity.status(HttpStatus.OK).body(reponse);
+        }
+    }
 
     @GetMapping("")
     public String deptHi() {
