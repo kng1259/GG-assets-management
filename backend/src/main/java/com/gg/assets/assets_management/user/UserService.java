d6e89bf9d7b54ef9503e88326a2d17ed3f8d43cb
kng1259 <khoinguyenanlac1@gmail.com>
2024-11-03 14:37:14 +0700

    add user add asset route

--- backend/src/main/java/com/gg/assets/assets_management/user/UserService.java
+++ backend/src/main/java/com/gg/assets/assets_management/user/UserService.java
@@ -5,11 +5,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gg.assets.assets_management.asset.Asset;
+import com.gg.assets.assets_management.asset.AssetRepository;
 
 @Service
 public class UserService {
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    AssetRepository assetRepository;
 
     public List<User> getAllUsers() {
         return userRepository.findAll();
@@ -17,5 +21,13 @@ public class UserService {
 
     public List<Asset> getAssetsByUserId(Long userId) {
         return userRepository.findById(userId).orElseThrow().getDepartment().getAssets();
+    }
+
+    public void addAssetToUser(Long userId, Asset asset) {
+        User user = userRepository.findById(userId).orElseThrow();
+        asset.setDepartment(user.getDepartment());
+        assetRepository.save(asset);
+        user.getDepartment().getAssets().add(asset);
+        userRepository.save(user);
     }
 }
